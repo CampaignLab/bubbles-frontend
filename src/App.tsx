8bb3fff160b89825268ad6dfd3fc791daa9f6c1d
Dan <daniel.stapleton24@gmail.com>
2026-02-10 10:59:06 +0000

    added preparing content screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { useEffect } from 'react';
 import DashboardPage from './features/dashboard/DashboardPage';
 
 function App() {
+  useEffect(() => {
+    // Add class to body to trigger splash screen fade out
+    document.body.classList.add('app-loaded');
+  }, []);
+
   return (
     <DashboardPage />
   )
